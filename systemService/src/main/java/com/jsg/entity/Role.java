c61ba5ce8efdf1525364da33c8110a590ca5aea7
jinsheng-fu <isleofu@126.com>
2019-10-11 10:29:33 +0800

    角色管理模块调试完成并测试通过

--- systemService/src/main/java/com/jsg/entity/Role.java
+++ systemService/src/main/java/com/jsg/entity/Role.java
@@ -1,7 +1,7 @@
 package com.jsg.entity;
 
-import lombok.Data;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import java.util.Date;
 
@@ -11,21 +11,21 @@ import java.util.Date;
  */
 @Data
 public class Role {
-    @ApiModelProperty("角色ID")
+    @ApiModelProperty(value = "角色ID")
     private Integer id;
     @ApiModelProperty(position = 1, value = "角色名称", required = true)
     private String name;
     @ApiModelProperty(position = 2, value = "角色编码", required = true)
     private String code;
-    @ApiModelProperty(position = 3, value = "描述",readOnly = true )
+    @ApiModelProperty(position = 3, value = "描述", readOnly = true)
     private String description;
-    @ApiModelProperty(position = 4, value = "权限数" ,readOnly = true )
+    @ApiModelProperty(position = 4, value = "权限数", readOnly = true)
     private Integer permissionNum;
     @ApiModelProperty(position = 5, value = "状态：0-停用；1-启用", required = true)
     private Integer status;
-    @ApiModelProperty(position = 6, value = "创建时间",readOnly = true )
+    @ApiModelProperty(position = 6, value = "创建时间", readOnly = true)
     private Date createTime = new Date();
-    @ApiModelProperty(position = 7, value = "修改时间" ,readOnly = true )
+    @ApiModelProperty(position = 7, value = "修改时间", readOnly = true)
     private Date updateTime = new Date();
     @ApiModelProperty(position = 8, value = "创建人", required = true)
     private Integer createUserId;
